72df9adb44f3420de9383e7a3a42784f950419b7
osawa-koki <koki@osawa.cloud>
2025-02-03 22:01:25 +0900

    オレオレ証明書の作成

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # oreore-certs
 
 🎃🎃🎃 オレオレ証明書を作成・使用してみる！  
+
+## オレオレ証明書を作成する
+
+```bash
+docker compose up --build -d
+```
+
+
---
+++ create.sh
@@ -0,0 +1,8 @@
+
+KEY_BITS=2048
+DAYS_VALID=365
+COMMON_NAME=localhost
+
+openssl genpkey -algorithm RSA -out $CERT_KEY_PATH -pkeyopt rsa_keygen_bits:$KEY_BITS \
+  && openssl req -new -key $CERT_KEY_PATH -out $CERT_CSR_PATH -subj "/CN=$COMMON_NAME" \
+  && openssl x509 -req -days $DAYS_VALID -in $CERT_CSR_PATH -signkey $CERT_KEY_PATH -out $CERT_PEM_PATH
